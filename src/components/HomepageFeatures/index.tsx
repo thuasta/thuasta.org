b543446a44dbc0c7ab586cbf4539f5416dc35f7e
1330Petrel <wuqw23@mails.tsinghua.edu.cn>
2025-02-17 18:38:05 +0800

    add some translations

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,9 +1,7 @@
 import clsx from 'clsx';
+import Translate, { translate } from '@docusaurus/Translate';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
-import SoftwareSvg from '@site/static/img/software.svg';
-import HardwareSvg from '@site/static/img/hardware.svg';
-import InnovationSvg from '@site/static/img/innovation.svg';
 
 import React, { JSX } from 'react';
 
@@ -15,29 +13,35 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: '软件部',
-    Svg: SoftwareSvg,
+    title: translate({ "message": '软件部', "id": "homepage.features.software.title" }),
+    Svg: 'img/software.svg',
     description: (
       <p>
-        软件部负责人工智能挑战赛，学生节游戏平台，电子设计大赛平台的搭建测试，网站维护等工作，我们注重代码质量和团队协作，欢迎你的加入！
+        <Translate id="homepage.features.software.description">
+          软件部负责人工智能挑战赛，学生节游戏平台，电子设计大赛平台的搭建测试，网站维护等工作，我们注重代码质量和团队协作，欢迎你的加入！
+        </Translate>
       </p>
     ),
   },
   {
-    title: '硬件部',
-    Svg: HardwareSvg,
+    title: translate({ "message": '硬件部', "id": "homepage.features.hardware.title" }),
+    Svg: 'img/hardware.svg',
     description: (
       <p>
-        硬件部专注于硬件创新与开发，负责电子设计大赛，机器狗等比赛的开发，期待你的加入！
+        <Translate id="homepage.features.hardware.description">
+          硬件部专注于硬件创新与开发，负责电子设计大赛，机器狗等比赛的开发，期待你的加入！
+        </Translate>
       </p>
     ),
   },
   {
-    title: '学创部',
-    Svg: InnovationSvg,
+    title: translate({ "message": '学创部', "id": "homepage.features.innovation.title" }),
+    Svg: 'img/innovation.svg',
     description: (
       <p>
-        学创部负责对接学校的各类科研培养项目，如星火计划、挑战杯，与企业实验室对接，帮助同学们完成人生发展规划，欢迎你的加入！
+        <Translate id="homepage.features.innovation.description">
+          学创部负责对接学校的各类科研培养项目，如星火计划、挑战杯，与企业实验室对接，帮助同学们完成人生发展规划，欢迎你的加入！
+        </Translate>
       </p>
     ),
   },
