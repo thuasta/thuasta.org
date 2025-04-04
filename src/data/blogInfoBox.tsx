68920a7325bb085144d5ee7d08d2b10db7c0c209
1330Petrel <wuqw23@mails.tsinghua.edu.cn>
2025-04-04 15:26:27 +0800

    add ProfileCard component

--- src/data/blogInfoBox.tsx
+++ src/data/blogInfoBox.tsx
@@ -3,8 +3,7 @@ import InfoBox, { type InfoBoxProps } from '../components/InfoBox';
 const BLOG_DATA: InfoBoxProps = {
     imageUrl: require('@site/static/img/zidonghuayuQRCode.png').default,
     alt: '紫冬话语',
-    title: '自动化系学生宣传平台',
-    description: '欢迎关注紫冬话语',
+    title: <>自动化系学生宣传平台<br />欢迎关注紫冬话语</>,
     imageWidth: 20,
 }
 
